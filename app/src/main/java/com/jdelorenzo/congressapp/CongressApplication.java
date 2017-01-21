7aa792399eff38f66f56c60d9d1f51bf9ba68c29
delorenzo <delorenzo.julie@gmail.com>
2017-01-21 11:11:17 -0500

    settings implementation

--- app/src/main/java/com/jdelorenzo/congressapp/CongressApplication.java
+++ app/src/main/java/com/jdelorenzo/congressapp/CongressApplication.java
@@ -7,19 +7,25 @@ import com.jdelorenzo.congressapp.network.NetComponent;
 import com.jdelorenzo.congressapp.network.NetModule;
 
 public class CongressApplication extends Application {
-    private NetComponent mNetComponent;
+    private NetComponent netComponent;
+    private AppComponent appComponent;
 
     @Override
     public void onCreate() {
         super.onCreate();
-
-        mNetComponent = DaggerNetComponent.builder()
-                .appModule(new AppModule(this))
+        AppModule appModule = new AppModule(this);
+        netComponent = DaggerNetComponent.builder()
+                .appModule(appModule)
                 .netModule(new NetModule(getString(R.string.base_api_url)))
+                .build();
+        appComponent = DaggerAppComponent.builder()
+                .appModule(appModule)
                 .build();
     }
 
     public NetComponent getNetComponent() {
-        return mNetComponent;
+        return netComponent;
     }
+
+    public AppComponent getAppComponent() { return appComponent; }
 }
--- app/src/main/java/com/jdelorenzo/congressapp/settings/AppCompatPreferenceActivity.java
+++ app/src/main/java/com/jdelorenzo/congressapp/settings/AppCompatPreferenceActivity.java
@@ -7,10 +7,12 @@ import android.preference.PreferenceFragment;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatDelegate;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -29,11 +31,14 @@ public class AppCompatPreferenceActivity extends PreferenceActivity {
         getDelegate().installViewFactory();
         getDelegate().onCreate(savedInstanceState);
         super.onCreate(savedInstanceState);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getFragmentManager()
                 .beginTransaction()
                 .replace(android.R.id.content, new CongressPreferenceFragment())
                 .commit();
     }
+
+
 
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
@@ -47,6 +52,15 @@ public class AppCompatPreferenceActivity extends PreferenceActivity {
 
     public void setSupportActionBar(@Nullable Toolbar toolbar) {
         getDelegate().setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            NavUtils.navigateUpFromSameTask(this);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
